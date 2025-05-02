34073453c2b47a6dc82542739d9e67518e9fa843
Will Wallace <wwallac1@trinity.edu>
2025-05-01 21:17:27 -0500

    Rate limiting and DoS detector

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import threading
 import socket
-import psycopg2
 import mydb
+import time
 
 host = "" # localhost
 port = 55555
+MAX_CONNECTIONS = 20
+MAX_CONNECTIONS_PER_MINUTE = 2
+connection_history = {} # Store connections timestamps for each IP
+banned_ips = []
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
@@ -51,6 +55,35 @@ def receive():
     try:
         while not serverClosed.is_set():
             client, address = server.accept()
+            client_ip = address[0]
+
+            # Check if user is allowed to join
+            # Refuse connection from banned users
+            if client_ip in banned_ips:
+                client.send("STOP".encode("ascii"))
+                continue
+
+            # Enforce connection limit (DoS protection)
+            if len(clients) >= MAX_CONNECTIONS:
+                client.send("[Server Busy] Too many connections. Try again later.\n".encode("ascii"))
+                time.sleep(0.2)
+                client.send("STOP".encode("ascii"))
+                continue
+
+            # Check if this ip has exceeded connection threshold
+            current_time = time.time()
+            if client_ip in connection_history:
+                connection_history[client_ip].append(current_time)
+                recent_connections = [t for t in connection_history[client_ip] if current_time - t < 60]
+                if len(recent_connections) >= MAX_CONNECTIONS_PER_MINUTE:
+                    print(f"DoS attack detected from IP: {client_ip}. Banning IP from server.")
+                    client.send("STOP".encode("ascii"))
+                    # Blacklist/ban IP
+                    banned_ips.append(client_ip)
+                    continue
+            else:
+                connection_history[client_ip] = [current_time]
+
             print(f"Connected with {str(address)}")
 
             client.send("NICK".encode("ascii"))
@@ -94,7 +127,7 @@ def write():
 # reverses the order, and concatenates them into a string
 # separated by \n
 def messageHistoryString(messages):
-    if(not messages):
+    if not messages:
         return ""
     history = ""
     for message in messages[::-1]:
